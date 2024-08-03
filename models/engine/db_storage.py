f3902eed17bafa07c9b99d738a8cae43e2d1296c
Christiana Apinoko <christyapinoko@gmail.com>
2024-08-03 16:07:32 +0000

    ad a get method to db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,8 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Return an object with matching id or None"""
+        if cls not in classes.values():
+            return None
